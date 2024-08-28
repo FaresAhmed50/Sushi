b4da7fa8fc8b26194a2e605c599a32e61d75252c
fares ahmed <fares.ahmed15967@aiet.edu.eg>
2024-08-29 02:06:03 +0300

    Finsing all sections

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 // import sushi11 from '../assets/sushi-11.png'
 // import sushi10 from '../assets/sushi-10.png'
 
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // init AOS animation
 AOS.init({
-    duration: 1000,
+    duration: 2000,
     offset: 100,
 });
 
